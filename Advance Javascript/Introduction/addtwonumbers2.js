d322fd94a0326ec9f44425e929cfe5dd88414b1f
sawsank <shasank.joshi@gmail.com>
2023-12-19 21:51:06 +0545

    committed

---
+++ Advance Javascript/Introduction/addtwonumbers2.js
@@ -0,0 +1,7 @@
+//store input numbers
+const num3 = parseInt(prompt('Enter the first numbwre: '));
+const num4 = parseInt(prompy('Enter the second number: '));
+
+const sum1 = num3 + num4;
+
+console.log(`the sum of ${num3} and ${num4} is ${sum1}`);
---
+++ Advance Javascript/Introduction/helloWorld.js
@@ -0,0 +1,8 @@
+//using comsole.log
+console.log('hello world');
+
+//using alert - for displaying asa alert in browser
+//alert("hello world");
+
+//using document.write - for displaying in browser
+//document.write("helloworld");
