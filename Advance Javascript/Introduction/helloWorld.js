c1256885d77079a89a41b4b3576d8a17e5c68de9
sawsank <shasank.joshi@gmail.com>
2023-12-19 21:52:09 +0545

    committed

--- Advance Javascript/Introduction/helloWorld.js
+++
@@ -1,8 +0,0 @@
-//using comsole.log
-console.log('hello world');
-
-//using alert - for displaying asa alert in browser
-//alert("hello world");
-
-//using document.write - for displaying in browser
-//document.write("helloworld");
